1bd6530b47eeda2186814e7da85e315c2ca94efb
Brett Lampson <brett.n.lampson@gmail.com>
2019-01-21 20:10:56 -0800

    Fizz Buzz

---
+++ Python_FizzBuzz.py
@@ -0,0 +1,18 @@
+
+
+# ------------------------------------------------------------------------------------------------ #
+# FIZZ BUZZ
+
+# def fizzbuzz(n):
+#     if n % 3 == 0 and n % 5 == 0:
+#         return 'FizzBuzz'
+#     elif n % 3 == 0:
+#         return 'Fizz'
+#     elif n % 5 == 0:
+#         return 'Buzz'
+#     else:
+#         return str(n)
+
+# print('--'.join(fizzbuzz(n) for n in range(1, 16)))
+# print('--'.join(fizzbuzz(n) for n in range(1, 16)))
+
