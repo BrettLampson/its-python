39a692cee69bea8d162c0a412e6e1fecd5f7073e
Brett Lampson <brett.n.lampson@gmail.com>
2019-02-02 21:11:12 -0800

    Web Scraping

--- Python_Web_Scraping.py
+++ Python_Web_Scraping.py
@@ -4,39 +4,39 @@
 # WEB SCRAPING
 
 
-print('\n# ' + '-'*80 + ' #')
+print('\n# ' + '-'*40 + ' #')
 # ------------------------------------------------------------------------------------------------ #
 # URLLIB library
 
 from urllib.request import urlopen
 
 # urlopen('url') is used to open a remote object across a network and read it.
-html = urlopen('http://pythonscraping.com/pages/page1.html')
+html = urlopen('https://news.ycombinator.com')
 
 # It can read: HTML files, image files, other files.
 print(html.read())
 
 
-print('\n# ' + '-'*80 + ' #')
+# print('\n# ' + '-'*40 + ' #')
 # ------------------------------------------------------------------------------------------------ #
 # BEAUTIFUL SOUP v4.0
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html, 'html.parser')
-print(bs)
-print('Getting Specific Tags')
-print(bs.title)
-print(bs.h1)
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+#
+# html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+# bs = BeautifulSoup(html, 'html.parser')
+# print(bs)
+# print('Getting Specific Tags')
+# print(bs.title)
+# print(bs.h1)
 
 
 # Note this returns only the first instance of h1 tag found on the page.
 # bs4 doesnt need .read() after the html either
 
 
-print('\n# ' + '-'*80 + ' #')
+print('\n# ' + '-'*40 + ' #')
 # ------------------------------------------------------------------------------------------------ #
 # POPULAR PARSERS
 # html.parser  (bs = BeautifulSoup(html, 'html.parser')
