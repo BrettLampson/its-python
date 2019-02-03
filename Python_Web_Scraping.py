b5dbde11fbe4a5586fe271bb2947af414f7cd300
Brett Lampson <brett.n.lampson@gmail.com>
2019-02-02 22:28:17 -0800

    Web Scraping

--- Python_Web_Scraping.py
+++ Python_Web_Scraping.py
@@ -8,28 +8,30 @@ print('\n# ' + '-'*40 + ' #')
 # ------------------------------------------------------------------------------------------------ #
 # URLLIB library
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
 
 # urlopen('url') is used to open a remote object across a network and read it.
-html = urlopen('https://news.ycombinator.com')
+# html = urlopen('https://news.ycombinator.com')
 
 # It can read: HTML files, image files, other files.
-print(html.read())
+# print(html.read())
 
 
 # print('\n# ' + '-'*40 + ' #')
 # ------------------------------------------------------------------------------------------------ #
 # BEAUTIFUL SOUP v4.0
 
-# from urllib.request import urlopen
-# from bs4 import BeautifulSoup
-#
-# html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-# bs = BeautifulSoup(html, 'html.parser')
-# print(bs)
-# print('Getting Specific Tags')
-# print(bs.title)
-# print(bs.h1)
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+
+bs = BeautifulSoup(html, 'html.parser')
+print(bs)
+
+# Getting Specific Tags
+print(bs.title)
+print(bs.h1)
 
 
 # Note this returns only the first instance of h1 tag found on the page.
